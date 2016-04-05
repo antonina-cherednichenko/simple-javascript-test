35857ad1642d49ddc9120a918f901472ddac1172
tonya <antonina.cherednichenko@gmail.com>
2016-04-05 15:15:29 +0300

    Init repo

--- log.js
+++ log.js
@@ -1,4 +1,12 @@
+var fs = require('fs');
+var program = require('commander');
+var tern = require('tern');
+var u = require('underscore');
 
 var message = function() {
   console.error('Hello');
+  fs.readFileSync("node_modules/tern/defs/ecma5.json", "utf8");
+  program.commandHelp();
+  var serv = new tern.Server({});
+  u.uniq([5, 3, 5, 5, 3, 1]);
 }
